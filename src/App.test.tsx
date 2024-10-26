900dff57fc05c4e983cc00a3332a5532f16c749d
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-10-26 15:32:32 -0400

    Add ability scores

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -13,3 +13,27 @@ it('has a "Name" input you can type in', () => {
 
   expect(nameInput).toHaveValue('Chalconte bar Irrisun');
 });
+
+describe('ability scores', () => {
+  it('has inputs for ability scores, defaulting to 10', () => {
+    render(<App />);
+
+    ['Str', 'Dex', 'Con', 'Int', 'Wis', 'Cha'].forEach((score) => {
+      const scoreInput = screen.getByLabelText(score);
+
+      expect(scoreInput).toHaveValue('10');
+    });
+  });
+
+  it('lets you edit the ability scores', () => {
+    render(<App />);
+
+    const strInput = screen.getByLabelText('Str');
+
+    fireEvent.focus(strInput);
+    fireEvent.change(strInput, { target: { value: '13' } });
+    fireEvent.blur(strInput);
+
+    expect(strInput).toHaveValue('13');
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import ModifiableField from './ModifiableField';
 function App() {
   const [fields, setFields] = useState({
     name: '',
-  } as Record<string, string>);
+    abilityScores: {
+      Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10,
+    } as Record<string, number>,
+  });
 
   return (
     <div className="App">
@@ -17,6 +20,28 @@ function App() {
           setFields({ ...fields, name: f });
         }}
       />
+
+      <div>
+        {
+          // TODO: check if there's a good way to make sure the order is correct
+          (Object.keys(fields.abilityScores)).map((score) => (
+            <ModifiableField
+              key={score}
+              value={fields.abilityScores[score].toString()}
+              formula={fields.abilityScores[score].toString()}
+              name={score}
+              setFormula={(f) => {
+                setFields({
+                  ...fields,
+                  abilityScores: {
+                    ...fields.abilityScores, [score]: parseInt(f, 10),
+                  },
+                });
+              }}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 }
