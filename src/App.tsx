d52a9f936a362bf67504a3fbf9ef4f7cc7ba4ea7
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-10-28 22:35:04 -0400

    Revert "Refactor: split up App state"
    This reverts commit c322b4a4151656a81d6a6cfeb952f1295a104c22.

--- src/App.tsx
+++ src/App.tsx
@@ -3,80 +3,91 @@ import './App.css';
 import { formatModifier, modFromAbilityScore } from './utils';
 import ModifiableField from './ModifiableField';
 
-const SAVES = [
+const saves = [
   { name: 'Fort', abilityScore: 'Con' },
   { name: 'Ref', abilityScore: 'Dex' },
   { name: 'Will', abilityScore: 'Wis' },
 ];
 
 function App() {
-  const [name, setName] = useState('')
-  const [abilityScores, setAbilityScores] = useState({
-    Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10,
-  } as Record<string, number>);
-  const [saves, setSaves] = useState({
-    Fort: { base: 0, total: 0 }, Ref: { base: 0, total: 0 }, Will: { base: 0, total: 0 },
-  } as Record<string, { base: number, total: number }>)
+  const [fields, setFields] = useState({
+    name: '',
+    abilityScores: {
+      Str: 10, Dex: 10, Con: 10, Int: 10, Wis: 10, Cha: 10,
+    } as Record<string, number>,
+    saves: {
+      Fort: { base: 0, total: 0 }, Ref: { base: 0, total: 0 }, Will: { base: 0, total: 0 },
+    } as Record<string, { base: number, total: number }>,
+  });
 
   return (
     <div className="App">
       <ModifiableField
-        value={name}
-        formula={name}
+        value={fields.name}
+        formula={fields.name}
         name="Name"
-        setFormula={(name) => { setName(name) }}
+        setFormula={(f) => {
+          setFields({ ...fields, name: f });
+        }}
       />
 
       <div id="ability-scores">
         {
           // TODO: check if there's a good way to make sure the order is correct
-          (Object.keys(abilityScores)).map((scoreName) => (
+          (Object.keys(fields.abilityScores)).map((scoreName) => (
             <div key={scoreName}>
               <ModifiableField
                 numeric
-                value={abilityScores[scoreName].toString()}
-                formula={abilityScores[scoreName].toString()}
+                value={fields.abilityScores[scoreName].toString()}
+                formula={fields.abilityScores[scoreName].toString()}
                 name={scoreName}
                 setFormula={(f) => {
-                  setAbilityScores({
-                    ...abilityScores, [scoreName]: parseInt(f, 10),
+                  setFields({
+                    ...fields,
+                    abilityScores: {
+                      ...fields.abilityScores, [scoreName]: parseInt(f, 10),
+                    },
                   });
                 }}
               />
-              <b>{formatModifier(modFromAbilityScore(abilityScores[scoreName]))}</b>
+              <b>{formatModifier(modFromAbilityScore(fields.abilityScores[scoreName]))}</b>
             </div>
           ))
         }
       </div>
 
       <div id="saves">
-        {SAVES.map(({ name, abilityScore }) => {
-          const abilityMod = modFromAbilityScore(abilityScores[abilityScore])
-          return (
-            <div key={name}>
-              <ModifiableField
-                numeric
-                value={(saves[name].base + abilityMod).toString()}
-                formula={(saves[name].base + abilityMod).toString()}
-                name={name}
-                setFormula={() => { }}
-              />
+        {saves.map(({ name, abilityScore }) => (
+          <div key={name}>
+            <ModifiableField
+              numeric
+              value={
+                (fields.saves[name].base + modFromAbilityScore(fields.abilityScores[abilityScore])).toString()
+              }
+              formula={
+                (fields.saves[name].base + modFromAbilityScore(fields.abilityScores[abilityScore])).toString()
+              }
+              name={name}
+              setFormula={() => { }}
+            />
 
-              <ModifiableField
-                numeric
-                value={saves[name].base.toString()}
-                formula={saves[name].base.toString()}
-                name="Base"
-                id={`base-${name}`}
-                setFormula={(f) => {
-                  setSaves({
-                    ...saves, [name]: { base: parseInt(f, 10), total: 0 },
-                  });
-                }}
-              />
-            </div>
-          )
-        })}
+            <ModifiableField
+              numeric
+              value={fields.saves[name].base.toString()}
+              formula={fields.saves[name].base.toString()}
+              name="Base"
+              id={`base-${name}`}
+              setFormula={(f) => {
+                setFields({
+                  ...fields,
+                  saves: {
+                    ...fields.saves, [name]: { base: parseInt(f, 10), total: 0 },
+                  },
+                });
+              }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
